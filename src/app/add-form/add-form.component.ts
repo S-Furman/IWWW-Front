9283367d734bdede6efca23532297c56e9929676
S-Furman <szymon.fur@o2.pl>
2023-01-18 16:37:05 +0100

    add form validation

--- src/app/add-form/add-form.component.ts
+++ src/app/add-form/add-form.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {AddressService} from "../address.service";
 
 @Component({
@@ -9,21 +9,23 @@ import {AddressService} from "../address.service";
 })
 export class AddFormComponent {
   address = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    street: new FormControl(''),
-    number: new FormControl(''),
-    city: new FormControl(''),
-    country: new FormControl('')
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    street: new FormControl('', [Validators.required]),
+    number: new FormControl('', [Validators.required]),
+    city: new FormControl('', [Validators.required]),
+    country: new FormControl('', [Validators.required])
   })
 
   constructor(private addressService: AddressService) { }
 
   onSubmit() {
-    this.addressService.addAddress(this.address.getRawValue()).subscribe(() => {
-      this.address.reset();
-      this.addressService.updateAddressesList();
-    });
+    if (this.address.valid) {
+      this.addressService.addAddress(this.address.getRawValue()).subscribe(() => {
+        this.address.reset();
+        this.addressService.updateAddressesList();
+      });
+    }
   }
 
   resetForm() {
